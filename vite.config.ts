51ab43fc4d846c992ca79810ff401bdb4ec73988
felipedavs <felipe@davs.com.br>
2025-05-13 18:42:16 -0300

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
-
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-}));
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/purple-yellow-web/', // nome do repositório com barra
-  ...
+  base: '/purple-yellow-web/',
+  plugins: [react()],
+})
