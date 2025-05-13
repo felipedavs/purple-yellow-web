50a3a6e18f46dc0c4095240d36fd96406d29245e
felipedavs <felipe@davs.com.br>
2025-05-13 19:04:47 -0300

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   base: '/purple-yellow-web/',
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
